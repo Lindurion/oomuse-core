1c059a5680ba2f34c0bda538b2a5d2efed64cea9
Eric W. Barndollar <eric@EricWBarndollar.com>
2016-12-10 13:24:34 -0800

    Improves conan.io build configuration
    - Depends on gtest as a static library (since it produces compiler
    warnings as a shared library).
    
    - Organizes and comments on conanfile.py to explain what is going on
    clearly and differentiate between configuration to build oomuse-core
    itself vs. configuration for clients to depend on it.

--- conanfile.py
+++ conanfile.py
@@ -16,35 +16,67 @@ import conans
 
 
 class OOMuseCoreConan(conans.ConanFile):
+  """The conan.io + CMake build configuration for C++ oomuse-core package."""
+
+  #=============================================================================
+  # Static configuration data about this package
+  #=============================================================================
+
   name = "oomuse-core"
   version = "0.1"
+
+  author = "Eric W. Barndollar (eric@EricWBarndollar.com)"
+  build_policy = "missing"  # Automatically build from source when not in cache.
+  license = "Apache 2.0"
+  settings = "os", "compiler", "build_type", "arch"  # Yield different binaries.
+  url = "https://github.com/lindurion/oomuse-core"
+
+
+  #=============================================================================
+  # Configuration options
+  #=============================================================================
 
   options = {
     "include_pdbs": [False, True],
     "testing": [False, True],
   }
-  default_options = "include_pdbs=False", "testing=False"
 
-  author = "Eric W. Barndollar (eric@EricWBarndollar.com)"
-  build_policy = "missing"
-  exports = "*"
-  generators = "cmake"
-  license = "Apache 2.0"
-  settings = "os", "compiler", "build_type", "arch"
-  url = "https://github.com/lindurion/oomuse-core"
+  # Note: No configuration option is provided for Windows static C runtime (the
+  # /MT or /MTd cl compiler options), so any package that uses this library must
+  # use the shared C runtime library on Windows (/MD or /MDd cl options).
+
+  # Note that gtest in shared mode produces compiler warnings, so link against
+  # as a static library so this build can treat warnings as errors.
+  default_options = "include_pdbs=False", "testing=False", "gtest:shared=False"
+
+
+  #=============================================================================
+  # Dependencies needed to build, run, and (optionally) test this package
+  #=============================================================================
 
   def requirements(self):
+    """Defines the conan.io packages required to build & test this package."""
     if self.options.testing:
       self.requires("gtest/1.8.0@lasote/stable")
 
   def imports(self):
+    """Copies dynamic libs from deps needed to run & test this package."""
     self.copy("*.dll", dst="bin", src="bin")
     self.copy("*.dylib*", dst="bin", src="lib")
 
-  def build(self):
-    cmake_test_def = "-DOOMUSE_CORE_TESTING=1" if self.options.testing else ""
 
+  #=============================================================================
+  # Building and (optionally) testing this package
+  #=============================================================================
+
+  generators = "cmake"  # Generate info needed to build against conan.io deps.
+
+  def build(self):
+    """Builds this package and optionally runs tests for it."""
+    # Build this package using CMake.
     cmake = conans.CMake(self.settings)
+
+    cmake_test_def = "-DOOMUSE_CORE_TESTING=1" if self.options.testing else ""
     self.run("cmake %s %s %s" % (self.conanfile_directory,
                                  cmake.command_line,
                                  cmake_test_def))
@@ -54,19 +86,29 @@ class OOMuseCoreConan(conans.ConanFile):
     if self.options.testing:
       self.run("ctest -C %s --output-on-failure" % self.settings.build_type)
 
+
+  #=============================================================================
+  # Configuring this built package for clients to depend on and use
+  #=============================================================================
+
+
   def package(self):
+    """Copies build outputs that client needs to be able to use this library."""
+
     # Include headers:
-    self.copy(pattern="*.h", dst="include", src="include", keep_path=True)
+    self.copy("*.h", dst="include", src="include", keep_path=True)
 
     # Static libs:
-    self.copy(pattern="*.a", dst="lib", src=".", keep_path=False)
-    self.copy(pattern="*.lib", dst="lib", src=".", keep_path=False)
+    self.copy("*.a", dst="lib", src=".", keep_path=False)
+    self.copy("*.lib", dst="lib", src=".", keep_path=False)
 
     # Debug symbols:
     if self.settings.compiler == "Visual Studio" and self.options.include_pdbs:
-      self.copy(pattern="*.pdb", dst="lib", src=".", keep_path=False)
+      self.copy("*.pdb", dst="lib", src=".", keep_path=False)
 
   def package_info(self):
+    """Configures client build as needed to depend on this library."""
+
     # Projects that use oomuse-core must link the right static library name.
     if self.settings.build_type == "Debug":
       self.cpp_info.libs.append("oomuse-core-debug")
